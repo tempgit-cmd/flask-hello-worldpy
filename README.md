47ff246129c708992bb214fbddf7f1e23e938105
tempgit-cmd <shubhthegrt21@gmail.com>
2026-01-19 17:21:03 +0530

    Added 1 to 10 numbers print

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-Flask Hello World application
-=============================
+Flask Hello World + 1 to 10 numbers print application
+======================================================
 
-This is the Flask Hello World application shamelessly not copied from
+This was the Flask Hello World application but shamelessly modified by the great minds to print from 1 to 10 from
+
 http://flask.pocoo.org/docs/quickstart/.
+
+
+
+
+===============================================================
+
+Originally modified by : https://github.com/shubhranshu-pandey
--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
+# Shamelessly copied but also modfied to print numbers from 1 to 10 so the shameless owner of this repo could read and code better from http://flask.pocoo.org/docs/quickstart/
 
 from flask import Flask
 app = Flask(__name__)
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+    # Now you have some shame i have modified this to also print 1 to 10!
+    numbers = " ".join(str(i) for i in range(1, 11))
+    return f"Hello World! Here are the numbers: {numbers}"
 
 if __name__ == '__main__':
     app.run()
